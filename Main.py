c99cbf9590615f79a5b71a6c26bf34f12a335716
Vitor Hugo <140010807+vitorhugo8@users.noreply.github.com>
2023-10-27 11:01:38 -0300

    Update_Main.py
    Mudei algumas coisas no seu código apenas para simplificá-lo para iniciantes. E adicionei uma condição caso o número adicionado pelo usuário seja nulo

--- Main.py
+++ Main.py
@@ -1,27 +1,18 @@
-def mensagem(ano,string):
-    print("O ano",ano,string)
+# Estrutura Condicional -- código mais simples
+from datetime import date
+ano_analisado = int(input("Que ano gostaria de analisar?: "))
 
-def ehBissexto(ano):
-    if(ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0 ):
-        if(ano >= 2018):
-            mensagem(ano,"serah bissexto")
-        else:
-            mensagem(ano,"foi bissexto")
-    else:
-        mensagem(ano,"NAO eh bissexto")
-
-def contaDigitos(ano):
-    string = str(ano)
-    if(len(string) != 4):
-        print("Ano invalido")
-    else:
-        ehBissexto(ano)
-
-list = []
-list = input().split(" ")
-list = [int(i) for i in list]
-for x in list:
-    contaDigitos(x)
+if ano_analisado == 0:
+    ano_analisado = date.today().year  
+    # Quando o usuario responde o ano como 0, o computador indica se o ano atual é bissexto ou não
+if ano_analisado % 4 == 0:
+    print(f"O ano {ano_analisado} é Bissexto")
+elif ano_analisado % 100 != 0:
+    print(f"O ano {ano_analisado} é Bissexto")
+elif ano_analisado % 400 == 0:
+    print(f"O ano {ano_analisado} é Bissexto")
+else:
+    print(f"{ano_analisado} Não é Bissexto")
 
 
 
